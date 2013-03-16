88638aefdb1749a21fbfff19ff882bc090ee7fe4
Nathanael Noblet <nathanael@noblet.ca>
2013-03-16 10:18:15 -0600

    restful access

--- Controller/RestController.php
+++ Controller/RestController.php
@@ -2,23 +2,18 @@
 
 namespace NS\DistanceBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use FOS\RestBundle\Controller\FOSRestController;
 
-/**
-* @Route(prefix="/rest")
-*/
-class DistanceController extends Controller
+class RestController extends FOSRestController
 {
     /**
-     * @Route("/distance/{postalcode1}/{postalcode2}", requirements={"_method": "GET"})
      * @Rest\View
      */
-    public function getAction($postalcode1,$postalcode2)
+    public function getAction($postalcode1,$postalcode2,$unit)
     {
-        $distance = $this->getContainer()->get('ns_distance.calculator')->getDistanceBetweenPostalCodes($postalcode1,$postalcode2);
+        $distance = $this->get('ns_distance.calculator')->getDistanceBetweenPostalCodes($postalcode1,$postalcode2,$unit);
 
         return array('from' => $postalcode1,'to'=>$postalcode2,'distance'=>$distance);
     }
