00425848a75eb0e49e735f2c91f2ac3c0535ecd9
Nathanael D. Noblet <nathanael@noblet.ca>
2014-01-29 15:32:03 -0700

    Use ObjectManager instead of EntityManager Whitespace cleanup

--- Services/DistanceCalculator.php
+++ Services/DistanceCalculator.php
@@ -2,22 +2,20 @@
 
 namespace NS\DistanceBundle\Services;
 
-use \Doctrine\ORM\EntityManager;
+use \Doctrine\ORM\ObjectManager;
 
 class DistanceCalculator
 {
     private $em;
-    
-    public function __construct(EntityManager $em) 
+
+    public function __construct(ObjectManager $em) 
     {
         $this->em = $em;
     }
 
     /**
      * This routine calculates the distance between two points (given the
-     * latitude/longitude of those points). It is being used to calculate
-     * the distance between two zip codes or postal codes using our
-     * Zipcodeworld(tm) and Postalcodeworld(tm) products.
+     * latitude/longitude of those points). 
      * 
      * @param lat1 source latitude point
      * @param lon1 source longitude point
@@ -26,7 +24,7 @@ class DistanceCalculator
      * @param unit the output unit
      */
     public function getDistance($lat1, $lon1, $lat2, $lon2, $unit) 
-    { 
+    {
         $theta = $lon1 - $lon2; 
         $dist = rad2deg(acos( sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta)) )); 
         $miles = $dist * 60 * 1.1515;
@@ -39,7 +37,7 @@ class DistanceCalculator
         else
             return $miles;
     }
-    
+
     public function getDistanceBetweenPostalCodes($postal1, $postal2, $unit = 'K')
     {
         $postal1 = strtoupper(preg_replace('/\s+/', '', $postal1));
@@ -47,7 +45,7 @@ class DistanceCalculator
         {
             foreach($postal2 as &$p)
                 $p = strtoupper(preg_replace('/\s+/', '', $p));
-            
+
             $codes = array_merge(array($postal1),$postal2);
         }
         else
@@ -55,15 +53,15 @@ class DistanceCalculator
             $postal2 = strtoupper(preg_replace('/\s+/', '', $postal2));
             $codes = array($postal1,$postal2);
         }
-        
+
         $data = $this->em->getRepository('NSDistanceBundle:PostalCode')->getByCodes($codes);
         if(count($data) < 2)
             throw new \Exception("Unable to find postal code");
-        
+
         if(is_array($postal2))
         {
             $ret = array();
-                
+
             foreach($postal2 as $pcode)
             {
                 if(isset($data[$pcode]))
