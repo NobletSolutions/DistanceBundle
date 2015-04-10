458f4f1574427ccbf680e9c0c6118d8fedac26b4
Nathanael d. Noblet <nathanael@noblet.ca>
2015-04-10 15:26:01 -0600

    remove unit value

--- Controller/RestController.php
+++ Controller/RestController.php
@@ -8,12 +8,12 @@ use \Symfony\Component\HttpFoundation\Response;
 
 class RestController extends Controller
 {
-    public function getAction($postalcode1,$postalcode2,$unit)
+    public function getAction($postalcode1,$postalcode2)
     {
         if(strpos($postalcode2, ',') !== FALSE)
             $postalcode2 = explode (',', $postalcode2);
 
-        $distance = $this->get('ns_distance.calculator')->getDistanceBetweenPostalCodes($postalcode1,$postalcode2,$unit);
+        $distance = $this->get('ns_distance.calculator')->getDistanceBetweenPostalCodes($postalcode1,$postalcode2);
         return new Response(json_encode($distance),200,array('Content-Type'=>'application/json'));
     }
 }
