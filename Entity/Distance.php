4d8e7cbc3cd98ed457ec65343baa1ead12ca3cfe
Nathanael d. Noblet <nathanael@noblet.ca>
2015-04-10 11:33:30 -0600

    added a distance entity

---
+++ Entity/Distance.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace NS\DistanceBundle\Entity;
+
+/**
+ * Description of Distance
+ *
+ * @author gnat
+ */
+class Distance
+{
+    const KM            = 0;
+    const NAUTICAL_MILE = 1;
+    const MILE          = 2;
+
+    private $distance;
+
+    /**
+     *
+     * @param double $distanceInMiles
+     */
+    public function __construct($distanceInMiles = null)
+    {
+        $this->distance = array();
+        if(!is_null($distanceInMiles) && is_numeric($distanceInMiles)) {
+            $this->setDistance ($distanceInMiles);
+        }
+    }
+
+    /**
+     *
+     * @param string $unit
+     * @return double
+     * @throws \InvalidArgumentException
+     */
+    public function getDistance($unit = self::KM)
+    {
+        if(!isset($this->distance[$unit])){
+            throw new \InvalidArgumentException("Unit of measurement $unit is invalid");
+        }
+   
+        return $this->distance[$unit];
+    }
+
+    /**
+     *
+     * @param double $distanceInMiles
+     */
+    public function setDistance($distanceInMiles)
+    {
+        $this->distance[self::KM] = $distanceInMiles*1.609344;
+        $this->distance[self::NAUTICAL_MILE] = $distanceInMiles*0.8684;
+        $this->distance[self::MILE] = $distanceInMiles;
+    }
+}
