28f6d8fabebc689b1df362801820c1e4f824960f
Nathanael d. Noblet <nathanael@noblet.ca>
2015-05-01 22:04:20 -0600

    catch unknown postal code exception

--- Controller/RestController.php
+++ Controller/RestController.php
@@ -2,18 +2,25 @@
 
 namespace NS\DistanceBundle\Controller;
 
+use \NS\DistanceBundle\Exceptions\UnknownPostalCodeException;
 use \Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use \Symfony\Component\HttpFoundation\Response;
 
-
 class RestController extends Controller
 {
-    public function getAction($postalcode1,$postalcode2)
+    public function getAction($postalcode1, $postalcode2)
     {
-        if(strpos($postalcode2, ',') !== FALSE)
-            $postalcode2 = explode (',', $postalcode2);
+        if (strpos($postalcode2, ',') !== FALSE) {
+            $postalcode2 = explode(',', $postalcode2);
+        }
 
-        $distance = $this->get('ns_distance.calculator')->getDistanceBetweenPostalCodes($postalcode1,$postalcode2);
-        return new Response(json_encode($distance),200,array('Content-Type'=>'application/json'));
+        try {
+            $distance = $this->get('ns_distance.calculator')->getDistanceBetweenPostalCodes($postalcode1, $postalcode2);
+        }
+        catch (UnknownPostalCodeException $exception) {
+            $distance = array();
+        }
+
+        return new Response(json_encode($distance), 200, array('Content-Type' => 'application/json'));
     }
 }
