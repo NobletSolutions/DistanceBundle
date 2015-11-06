cf6f6ac45aba35bef5d320c8c8e55b50018f75f0
Nathanael d. Noblet <nathanael@noblet.ca>
2015-11-06 12:50:34 -0700

    added new postal code checker service

---
+++ Services/PostalCodeChecker.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace NS\DistanceBundle\Services;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use NS\DistanceBundle\Entity\PostalCode;
+
+class PostalCodeChecker
+{
+    /**
+     * @var ObjectManager
+     */
+    private $entityMgr;
+
+    /**
+     * PostalCodeChecker constructor.
+     * @param $entityMgr
+     */
+    public function __construct(ObjectManager $entityMgr)
+    {
+        $this->entityMgr = $entityMgr;
+    }
+
+    /**
+     * @param $postalCode
+     * @return GeographicPointInterface|null
+     */
+    public function getLatitudeAndLongitude($postalCode)
+    {
+        $postalObj = $this->entityMgr->getRepository('NSDistanceBundle:PostalCode')->getByCode($postalCode);
+
+        if(!$postalObj) {
+            $url = sprintf('http://maps.googleapis.com/maps/api/geocode/json?components=postal_code:%s&sensor=false',$postalCode);
+            $result = file_get_contents($url);
+            $response = json_decode($result, true);
+
+            if ($response['status'] != 'OK') {
+                $lat = '';
+                $long = '';
+            } else {
+                $geometry = $response['results'][0]['geometry'];
+
+                $postalObj = new PostalCode();
+                $postalObj->setLongitude($geometry['location']['lat']);
+                $postalObj->setLatitude($geometry['location']['lng']);
+                $postalObj->setCity($response['results'][0]['address_components'][2]['short_name']);
+                $postalObj->setPostalCode($response['results'][0]['address_components'][4]['short_name']);
+                $this->entityMgr->persist($postalObj);
+            }
+        }
+
+        return $postalObj;
+    }
+}
+
