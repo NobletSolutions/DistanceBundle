5babc37f95a74fdf365436b5e95859ea9c96620a
Nathanael d. Noblet <nathanael@noblet.ca>
2017-01-17 00:11:42 -0700

    removed unused code, added and fixed test

--- Tests/Services/PostalCodeCheckerTest.php
+++ Tests/Services/PostalCodeCheckerTest.php
@@ -3,7 +3,42 @@
 namespace NS\DistanceBundle\Tests\Services;
 
 
+use Doctrine\Common\Persistence\ObjectManager;
+use NS\DistanceBundle\Entity\PostalCode;
+use NS\DistanceBundle\Repositories\PostalCodeRepository;
+use NS\DistanceBundle\Services\PostalCodeChecker;
+
 class PostalCodeCheckerTest extends \PHPUnit_Framework_TestCase
 {
+    private $entityManager;
+    private $repository;
 
+    public function testResultIsInDB()
+    {
+        $postCode = new PostalCode();
+        $this->repository->expects($this->once())->method('getByCode')->with('T3A5J4')->willReturn($postCode);
+        $service = new PostalCodeChecker($this->entityManager);
+        $this->assertEquals($postCode,$service->getLatitudeAndLongitude('T3A5J4'));
+    }
+
+    public function testResultIsNotInDB()
+    {
+        $this->repository->expects($this->once())->method('getByCode')->with('T3A5J4')->willReturn(null);
+        $this->entityManager->expects($this->once())->method('persist');
+        $this->entityManager->expects($this->once())->method('flush');
+
+        $service = new PostalCodeChecker($this->entityManager);
+        $postCode = $service->getLatitudeAndLongitude('T3A5J4');
+        $this->assertEquals('T3A5J4', $postCode->getPostalCode());
+        $this->assertEquals(-114.1077228,$postCode->getLatitude());
+        $this->assertEquals(51.149276800000003,$postCode->getLongitude());
+    }
+
+    protected function setUp()
+    {
+        $this->repository = $this->getMockBuilder(PostalCodeRepository::class)->disableOriginalConstructor()->getMock();
+
+        $this->entityManager = $this->getMockBuilder(ObjectManager::class)->disableOriginalConstructor()->getMock();
+        $this->entityManager->expects($this->once())->method('getRepository')->with('NSDistanceBundle:PostalCode')->willReturn($this->repository);
+    }
 }
--- src/Services/PostalCodeChecker.php
+++ src/Services/PostalCodeChecker.php
@@ -3,6 +3,7 @@
 namespace NS\DistanceBundle\Services;
 
 use Doctrine\Common\Persistence\ObjectManager;
+use NS\DistanceBundle\Entity\GeographicPointInterface;
 use NS\DistanceBundle\Entity\PostalCode;
 
 class PostalCodeChecker
@@ -30,15 +31,12 @@ class PostalCodeChecker
         $postalCode = strtoupper(preg_replace('/\s+/', '', $postalCode));
         $postalObj = $this->entityMgr->getRepository('NSDistanceBundle:PostalCode')->getByCode($postalCode);
 
-        if(!$postalObj) {
-            $url = sprintf('http://maps.googleapis.com/maps/api/geocode/json?components=postal_code:%s&sensor=false',$postalCode);
+        if (!$postalObj) {
+            $url = sprintf('http://maps.googleapis.com/maps/api/geocode/json?components=postal_code:%s&sensor=false', $postalCode);
             $result = file_get_contents($url);
             $response = json_decode($result, true);
 
-            if ($response['status'] != 'OK') {
-                $lat = '';
-                $long = '';
-            } else {
+            if ($response['status'] == 'OK') {
                 $geometry = $response['results'][0]['geometry'];
 
                 $postalObj = new PostalCode();
@@ -46,8 +44,10 @@ class PostalCodeChecker
                 $postalObj->setLatitude($geometry['location']['lng']);
                 $postalObj->setCity($response['results'][0]['address_components'][2]['short_name']);
                 $postalObj->setPostalCode($postalCode);
-                $postalObj->setProvince(isset($response['results'][0]['address_components'][4]['short_name'])? $response['results'][0]['address_components'][4]['short_name']: "AB");
+                $postalObj->setProvince(isset($response['results'][0]['address_components'][4]['short_name']) ? $response['results'][0]['address_components'][4]['short_name'] : "AB");
+
                 $this->entityMgr->persist($postalObj);
+                $this->entityMgr->flush($postalObj);
             }
         }
 
