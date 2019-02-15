d84464cb1e64b55569c131a8eb2d82e385fe2a86
Mark Woynarski <mark.woynarski@nobletdesign.com>
2019-02-15 16:04:08 -0600

    Use csv parser

--- src/Command/ImportPostalCodesCommand.php
+++ src/Command/ImportPostalCodesCommand.php
@@ -6,6 +6,7 @@ use \Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use \Symfony\Component\Console\Input\InputArgument;
 use \Symfony\Component\Console\Input\InputInterface;
 use \Symfony\Component\Console\Output\OutputInterface;
+use \KzykHys\CsvParser\CsvParser;
 
 class ImportPostalCodesCommand extends ContainerAwareCommand
 {
